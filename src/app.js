c407522751d5faa94907a046fc31fc45c8aeb1b2
FENG XU <f.xu@revers.io>
2024-08-02 18:18:13 +0200

    t

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 
 function getResult(){
-    return new Date().toDateString();
+    // return new Date().toDateString();
+    return 1+1
 }
 
 document.getElementById("result").innerHTML = getResult();
